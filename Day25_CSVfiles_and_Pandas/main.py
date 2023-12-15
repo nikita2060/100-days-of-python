41d1642af90c4094a333d53911f4f87d10975983
Nikita Pandey <94976186+nikita2060@users.noreply.github.com>
2023-12-15 15:25:46 +0530

    Update main.py

--- Day25_CSVfiles_and_Pandas/main.py
+++ Day25_CSVfiles_and_Pandas/main.py
@@ -1,18 +1,18 @@
-#import pandas as pd
-# import csv  # in python, there is a library for working with csv files as python is very useful in data related fields
+import pandas as pd
+import csv  # in python, there is a library for working with csv files as python is very useful in data related fields
 
-# with open("weather.csv") as file:
-#     weather_list = file.readlines()
-#     print(weather_list)
+with open("weather.csv") as file:
+    weather_list = file.readlines()
+    print(weather_list)
 
 
-# with open("weather.csv") as file:
-#     weather_list = csv.reader(file)
-#     temperature = []
-#     for row in weather_list:
-#         if row[1] != "temp":  # When I didn't write this line it was showing invalid literal means temp was string and
-#             # I was trying to make it int we cant make strings to int
-#             temperature.append(int(row[1]))
-#
-#     print(temperature)
+with open("weather.csv") as file:
+    weather_list = csv.reader(file)
+    temperature = []
+    for row in weather_list:
+        if row[1] != "temp":  # When I didn't write this line it was showing invalid literal means temp was string and
+            # I was trying to make it int we cant make strings to int
+            temperature.append(int(row[1]))
+
+    print(temperature)
 
