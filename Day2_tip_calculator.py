ca39dfcbe19ded7c31a66464cbad5e3e09daf47b
Nikita Pandey <94976186+nikita2060@users.noreply.github.com>
2024-06-06 11:51:37 +0530

    Update Day2_tip_calculator.py

--- Day2_tip_calculator.py
+++ Day2_tip_calculator.py
@@ -1,15 +1,9 @@
 #If the bill was $150.00, split between 5 people, with 12% tip. 
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Round the result to 2 decimal places.
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-people = int(input("How many people to split the bill?"))
-
-tip_as_percent = tip / 100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
-
-print(f"Each person should pay: ${final_amount}")
+print("Welcome to splitwise!\n")
+total_bill = float(input("Enter total bill amount (Rs.): Rs."))
+no_of_people = int(input("Enter number of people : "))
+tip = int(input("How much tip you want to give 'Rs.50', 'Rs.100', 'Rs.200' : Rs. " ))
+bill_for_each = round(((total_bill +tip) / no_of_people),2)
+print(f"Each person has to contribute Rs. {bill_for_each}")
